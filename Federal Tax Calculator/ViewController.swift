412e65404e1508f6a855cb9c7e1156a4583cd778
Sebastian Tyson <styson@lcs.on.ca>
2019-11-04 18:49:48 -0500

    Initial Commit

---
+++ Federal Tax Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Federal Tax Calculator
+//
+//  Created by Tyson, Sebastian on 2019-10-29.
+//  Copyright © 2019 Tyson, Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+        switch grossIncome{
+        case 0...50000
+        case 50001...100000
+        default:
+    }
+
+
+}
+
