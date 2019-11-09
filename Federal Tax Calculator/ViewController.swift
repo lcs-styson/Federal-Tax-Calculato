b21b26950006ed5d694867d630ba3543911a0001
Sebastian Tyson <styson@lcs.on.ca>
2019-11-09 13:41:40 -0500

    Is now functional

--- Federal Tax Calculator/ViewController.swift
+++ Federal Tax Calculator/ViewController.swift
@@ -10,29 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var nameInput: UITextField!
-    @IBOutlet weak var grossIncomeInput: UITextField!
-    @IBOutlet weak var yourAmount: UILabel!
+
+
+    @IBOutlet weak var taxesOwingOutput: UILabel!
+    @IBOutlet weak var incomeInput: UITextField!
     @IBOutlet weak var calculateAmount: UIButton!
+    @IBOutlet weak var effectiveTaxRateOutput: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-  
-       
-        guard let grossIncomeAsString = grossIncomeInput.text else {
- 
-        switch grossIncome {
         
-        case 0...50000:
-            yourAmount.text = grossIncome * 0.15
+        
+        
+        
+    }
+    
+    @IBAction func calculateTaxes(_ sender: Any) {
+        
+        // Get the input as a string
+        guard let incomeAsString = incomeInput.text else {
+            taxesOwingOutput.text = "Please enter an inocme."
+            return
+        }
+        
+        // Get the income as a Double
+        guard let incomeAsDouble = Double(incomeAsString) else {
+            return
+        }
+        
+        switch incomeAsDouble {
+        case 0...47630:
+            let taxesOwing = incomeAsDouble * 0.15
+            taxesOwingOutput.text = "You owe " + String(taxesOwing)
             
-        case 50001...100000:
-            yourAmount.text = grossIncome * 0.205
+//        case 47631...95295:
             
         default:
-            yourAmount.text = grossIncome * 0.26
+            taxesOwingOutput.text = "Need to add code here"
+        }
+        
     }
-
-
 }
-
